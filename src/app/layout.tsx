6f22b1c3a553eb377157336ea781c8a7645704ef
rudeuns <rudeuns@gmail.com>
2025-05-19 00:46:06 +0900

    feat: 루트 레이아웃에 토스트 메세지 위치 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Toaster } from '@/components/ui/sonner';
 import '@/globals.css';
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
       <body className="antialiased lg:bg-neutral-200">
         <main className="bg-background mx-auto min-h-dvh w-full overflow-y-auto lg:max-w-md">
           {children}
+          <Toaster position="top-center" />
         </main>
       </body>
     </html>
